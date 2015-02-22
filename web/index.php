4e919ef037f234e790e3d80d7c9d691ed78e3656
Juerg Hunziker <juerg.hunziker@gmail.com>
2015-02-22 15:56:15 +0100

    disabled user zooming

--- web/index.php
+++ web/index.php
@@ -8,7 +8,7 @@ $wrongpassword = isset($_GET['wrongpassword']) && $_GET['wrongpassword'] == 1;
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="description" content="Wedding page of Nadia und Martin">
     <meta name="author" content="Jürg Hunziker">
 
--- web/wedding.php
+++ web/wedding.php
@@ -20,7 +20,7 @@ if(isset($_GET['subscription'])) {
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="description" content="Wedding page of Nadia und Martin">
     <meta name="author" content="Jürg Hunziker">
 
