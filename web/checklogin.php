154c5ea013a67d125b4c20a85d61ef3d826b18c3
Juerg Hunziker <juerg.hunziker@gmail.com>
2015-07-30 22:03:31 +0200

    added thankyou password

--- web/checklogin.php
+++ web/checklogin.php
@@ -6,12 +6,15 @@ function checkLogin($password = '') {
     if(!empty($password)) {
         $viewAperoPassword = '093ef96957de80044663a930a551d089';
         $viewEsssenPassword = '1fbe25e6fff04c567326d9a79e2ab171';
+        $viewThankyouPassword = '801c2990fadc2960036bee72b13ac0ff';
 
         $hashedPassword = md5(strtolower($password));
         if($hashedPassword == $viewAperoPassword) {
             return 'apero';
         } elseif($hashedPassword == $viewEsssenPassword) {
             return 'essen';
+        } elseif($hashedPassword == $viewThankyouPassword) {
+            return 'thankyou';
         } else {
             header("Location: index.php?wrongpassword=1");
         }
